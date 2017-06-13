4008a96ad587e5082c6cd9c40950cfa4bd49702a
liwenli <102495553@qq.com>
2017-06-13 18:18:25 +0800

    update call+apply

---
+++ 第二周/Day1/utils.js
@@ -0,0 +1,24 @@
+/**
+ * Created by liwenli on 2017/6/13.
+ */
+var utils = (function () {
+    /**
+     * listToAry: 将类数组转化为数组
+     * @param likeAry 类数组
+     * @returns newAry 转化后的数组
+     */
+    function listToAry(likeAry) {
+        try {
+            return [].slice.call(likeAry);
+        }catch (e) {
+            var arr = [];
+            for(var i = 0; i < likeAry.length; i++){
+                arr.push(likeAry[i]);
+            }
+            return arr;
+        }
+    }
+    return {
+       listToAry: listToAry
+    }
+})();
